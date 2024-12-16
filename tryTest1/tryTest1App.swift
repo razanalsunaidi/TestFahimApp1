000b50396e1204888bbaf2671aa33f71ba32ba06
razanalsunaidi <razanalsunedi@gmail.com>
2024-12-16 14:18:35 +0300

    Initial Commit

---
+++ tryTest1/tryTest1App.swift
@@ -0,0 +1,17 @@
+//
+//  tryTest1App.swift
+//  tryTest1
+//
+//  Created by Razan Alsunaidi on 15/06/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct tryTest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tryTest1UITests/tryTest1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  tryTest1UITestsLaunchTests.swift
+//  tryTest1UITests
+//
+//  Created by Razan Alsunaidi on 15/06/1446 AH.
+//
+
+import XCTest
+
+final class tryTest1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
